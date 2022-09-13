6d432249d46766a5e9dfc3302f936b0832e3c129
田毅 <3305730188@qq.com>
2022-09-13 17:57:26 +0800

    修改Person

--- TestDemo01/src/main/java/com/yi/Person.java
+++ TestDemo01/src/main/java/com/yi/Person.java
@@ -3,4 +3,9 @@ package com.yi;
 public class Person {
     private String name;
     private int ages;
+    private int age;
+    private int age1;
+    private int age2;
+    private int age3;
+    private int age4;
 }
