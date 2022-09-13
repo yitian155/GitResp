461cab1b6e84b6d401a2d1faa407c334890e553d
田毅 <3305730188@qq.com>
2022-09-13 18:02:37 +0800

    修改person

--- TestDemo01/src/main/java/com/yi/Student.java
+++ TestDemo01/src/main/java/com/yi/Student.java
@@ -3,4 +3,8 @@ package com.yi;
 public class Student {
     private String name;
     private int ages;
+    private String name1;
+    private String name2;
+    private String name3;
+    private String name4;
 }
